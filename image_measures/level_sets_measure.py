098227d5a00e15dc6e7bc81d5a55f459cbc74c21
intsco <intscorpio@gmail.com>
2015-08-17 10:53:25 +0300

    Added missing import. Put some checks to measure_of_chaos_dict for input data.

--- image_measures/level_sets_measure.py
+++ image_measures/level_sets_measure.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy import ndimage, misc, ndarray, interpolate
+from scipy.signal import medfilt
+
 
 def measure_of_chaos(im,nlevels,interp=True,q_val = 99.):
 	''' Function for calculating a measure of image noise using level sets
@@ -27,6 +29,7 @@ def measure_of_chaos(im,nlevels,interp=True,q_val = 99.):
 		im_q = np.percentile(im_clean[notnull],q_val)
 		im_rep =  im_clean>im_q
 		im_clean[im_rep] = im_q
+
 		# interpolate to clean missing values
 		if interp == 'interpolate' or interp==True: # interpolate to replace missing data - not always present
 			try:
@@ -72,15 +75,22 @@ def measure_of_chaos(im,nlevels,interp=True,q_val = 99.):
 		bw=ndimage.morphology.binary_erosion(bw,structure=erode_mask)
 		# Record objects at this level
 		num_objs.append(ndimage.label(bw)[1])#second output is number of objects
+
 	measure_value = float(np.sum(num_objs))/(sum_notnull*nlevels)
 	return measure_value #,im,levels,num_objs
 
 
 def measure_of_chaos_dict(d, nRows, nColumns, nlevels=20, interp=True, q_val = 99.):
 	'''Applies :code:`measure_of_chaos` to an image given as a dictionary.'''
+	if len(d) == 0:
+		raise Exception('Empty input dict!')
+
+	max_key_val = nRows * nColumns - 1
 	iSize = nRows*nColumns
 	img = np.zeros((iSize,1))
 	for i,v in d.iteritems():
-		if i >= 0 and i < iSize:
-			img[i] = v
+		if i < 0 or i > max_key_val:
+			raise Exception('Wrong key value in input dict: {}!'.format(i))
+		# if i >= 0 and i < iSize:
+		img[i] = v
 	return measure_of_chaos(np.reshape(img,(nRows, nColumns)), nlevels, interp, q_val)
