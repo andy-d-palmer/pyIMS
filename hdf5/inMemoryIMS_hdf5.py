f5cdc750f8a7feec4b28aff10b646b3af3e6de69
palmer <andrew.palmer@embl.de>
2015-08-19 09:09:14 +0200

    parser determined min/max mz on data load

--- hdf5/inMemoryIMS_hdf5.py
+++ hdf5/inMemoryIMS_hdf5.py
@@ -60,6 +60,8 @@ class inMemoryIMS_hdf5():
         self.mz_list = self.mz_list[mz_order]
         self.count_list = self.count_list[mz_order]
         self.idx_list = self.idx_list[mz_order]
+        self.mz_min=self.mz_list[0]
+        self.mz_max=self.mz_list[-1]
         print 'file loaded'
     def get_coords(self):
         coords = np.zeros((len(self.index_list),3))
@@ -68,8 +70,19 @@ class inMemoryIMS_hdf5():
         return coords
     def get_spectrum(self,index):
         this_spectrum = mass_spectrum()
-        tmp_str='/spectral_data/%d/' %(index) 
-        this_spectrum.add_centroids(self.hdf[tmp_str+'/centroid_mzs/'],self.hdf[tmp_str+'/centroid_intensities/'])
+        tmp_str='/spectral_data/%d' %(index) 
+        try:
+            this_spectrum.add_spectrum(self.hdf[tmp_str+'/mzs/'],self.hdf[tmp_str+'/intensities/'])
+            got_spectrum=True
+        except KeyError:
+            got_spectrum=False
+        try:
+            this_spectrum.add_centroids(self.hdf[tmp_str+'/centroid_mzs/'],self.hdf[tmp_str+'/centroid_intensities/'])
+            got_centroids=True
+        except KeyError:
+            got_centroids=False
+        if not any([got_spectrum,got_centroids]):
+            raise ValueError('No spectral data found in index {}'.format(index))
         return this_spectrum
     def get_ion_image(self,mzs,tols,tol_type='ppm'):
         tols = tols*mzs/1e6 # to m/z
@@ -87,3 +100,41 @@ class inMemoryIMS_hdf5():
             ion_vect=np.bincount(idx_vect,count_vect)
             data_out.add_xic(ion_vect,[mz],[tol])
         return data_out
+        # Form histogram axis
+    def generate_histogram_axis(self,ppm=1.):
+        mz_current = self.mz_min
+        ppm_mult=ppm*1e-6
+        mz_list = []
+        while mz_current<self.mz_max:
+            mz_list.append(mz_current)
+            mz_current=mz_current+mz_current*ppm_mult
+        self.histogram_mz_axis = mz_list
+    def get_histogram_axis(self):
+        try:
+            mz_axis = self.histogram_mz_list()
+        except:
+            print 'generating histogram with default ppm (1)' 
+            self.generate_histogram_axis()
+        return self.histogram_mz_axis
+    def generate_summary_spectrum(self,summary_type='mean'):
+        hist_axis = self.get_histogram_axis()
+        # calcualte mean along some m/z axis
+        mean_spec=np.zeros(np.shape(hist_axis))
+        for ii in range(0,len(hist_axis)-1):   
+            mz_upper = hist_axis[ii+1]
+            mz_lower = hist_axis[ii]
+            idx_left = bisect.bisect_left(self.mz_list,mz_lower)
+            idx_right = bisect.bisect_right(self.mz_list,mz_upper)
+            # slice list for code clarity
+            count_vect = self.count_list[idx_left:idx_right]
+            if summary_type=='mean':
+                count_vect = self.count_list[idx_left:idx_right]
+                mean_spec[ii]=np.sum(count_vect)
+            elif summary_type=='freq':
+                idx_vect = self.idx_list[idx_left:idx_right]
+                mean_spec[ii]=float(len(np.unique(idx_vect)))
+        if summary_type=='mean':
+                mean_spec=mean_spec/len(self.index_list)
+        elif summary_type=='freq':
+                mean_spec=mean_spec/len(self.index_list)
+        return hist_axis,mean_spec
