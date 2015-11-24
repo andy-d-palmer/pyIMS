4d9b1bff7618f441a2f5c08c7d084898be40e597
Dominik Fay <dominik.fay@gmx.de>
2015-11-24 17:42:16 +0100

    Split level sets function out and reorder

--- image_measures/level_sets_measure.py
+++ image_measures/level_sets_measure.py
@@ -44,43 +44,22 @@ def measure_of_chaos(im, nlevels, interp='interpolate', q_val=99., overwrite=Tru
         raise ValueError('{}: interp option not recognised'.format(interp))
     im_clean = interp_func(im) / im_q
 
-    ## Level Sets Calculation
-    # calculate levels
-    levels = np.linspace(0, 1, nlevels)  # np.amin(im), np.amax(im)
-    # hardcoded morphology masks
-    dilate_mask = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
-    erode_mask = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-
-    # Go though levels and calculate number of objects
-    num_objs = []
-    for lev in levels:
-        # Threshold at level
-        bw = (im_clean > lev)
-        # Morphological operations
-        bw = ndimage.morphology.binary_dilation(bw, structure=dilate_mask)
-        bw = ndimage.morphology.binary_erosion(bw, structure=erode_mask)
-        # Record objects at this level
-        num_objs.append(ndimage.label(bw)[1])  # second output is number of objects
-        sum_vals = np.sum(num_objs)
-        if sum_vals == nlevels * num_objs[-1]:  # single pixel noise elimination
-            sum_vals = 0
-    measure_value = float(sum_vals) / (sum_notnull * nlevels)
-    return measure_value, im_clean, levels, num_objs
+    # Level Sets Calculation
+    return _level_sets(im_clean, nlevels, sum_notnull)
 
 
-def _interpolate(im, notnull_mask):
+def _nan_to_zero(im):
     """
-    Use spline interpolation to fill in missing values.
+    Set all values to zero that are less than zero or nan; return the indices of all elements that are zero after
+    the modification (i.e. those that have been nan or smaller than or equal to zero before calling this function). The
+    returned boolean array has the same shape, True denotes that a value is zero now.
 
-    :param im: the entire image, including nan or zero values
-    :param notnull_mask: the indices array for the values greater than zero
-    :return: the interpolated array
+    :param im: the array which nan-values will be set to zero in-place
+    :return: A boolean array of the same shape as :code:`im`
     """
-    im_size = im.shape
-    X, Y = np.meshgrid(np.arange(0, im_size[1]), np.arange(0, im_size[0]))
-    f = interpolate.interp2d(X[notnull_mask], Y[notnull_mask], im[notnull_mask])
-    im = f(np.arange(0, im_size[1]), np.arange(0, im_size[0]))
-    return im
+    notnull = im > 0  # does not include nan values
+    im[~notnull] = 0
+    return notnull
 
 
 def _quantile_threshold(im, notnull_mask, q_val):
@@ -98,16 +77,39 @@ def _quantile_threshold(im, notnull_mask, q_val):
     return im_q
 
 
-def _nan_to_zero(im):
+def _interpolate(im, notnull_mask):
     """
-    Set all values to zero that are less than zero or nan; return the indices of all elements that are zero after
-    the modification (i.e. those that have been nan or smaller than or equal to zero before calling this function). The
-    returned boolean array has the same shape, True denotes that a value is zero now.
+    Use spline interpolation to fill in missing values.
 
-    :param im: the array which nan-values will be set to zero in-place
-    :return: A boolean array of the same shape as :code:`im`
+    :param im: the entire image, including nan or zero values
+    :param notnull_mask: the indices array for the values greater than zero
+    :return: the interpolated array
     """
-    # Image properties
-    notnull = im > 0  # does not include nan values
-    im[~notnull] = 0
-    return notnull
+    im_size = im.shape
+    X, Y = np.meshgrid(np.arange(0, im_size[1]), np.arange(0, im_size[0]))
+    f = interpolate.interp2d(X[notnull_mask], Y[notnull_mask], im[notnull_mask])
+    im = f(np.arange(0, im_size[1]), np.arange(0, im_size[0]))
+    return im
+
+
+def _level_sets(im_clean, nlevels, sum_notnull=None):
+    # calculate levels
+    levels = np.linspace(0, 1, nlevels)  # np.amin(im), np.amax(im)
+    # hardcoded morphology masks
+    dilate_mask = [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
+    erode_mask = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    # Go though levels and calculate number of objects
+    num_objs = []
+    for lev in levels:
+        # Threshold at level
+        bw = (im_clean > lev)
+        # Morphological operations
+        bw = ndimage.morphology.binary_dilation(bw, structure=dilate_mask)
+        bw = ndimage.morphology.binary_erosion(bw, structure=erode_mask)
+        # Record objects at this level
+        num_objs.append(ndimage.label(bw)[1])  # second output is number of objects
+    sum_vals = np.sum(num_objs)
+    if sum_vals == nlevels * num_objs[-1]:  # all objects are in the highest level
+        sum_vals = 0
+    measure_value = float(sum_vals) / (sum_notnull * nlevels)
+    return measure_value, im_clean, levels, num_objs
