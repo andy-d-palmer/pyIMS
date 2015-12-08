c40fa841d492f896216900dd8d261b725a5f2b45
Dominik Fay <dominik.fay@gmx.de>
2015-12-08 19:30:04 +0100

    [sci] Apply more TODOs
    see the TODO comments in the code

--- image_measures/level_sets_measure.py
+++ image_measures/level_sets_measure.py
@@ -54,8 +54,7 @@ def measure_of_chaos(im, nlevels, interp='interpolate', q_val=99., overwrite=Tru
 
     # Level Sets Calculation
     object_counts = _level_sets(im_clean, nlevels)
-    # TODO: return the plain value instead of a tuple
-    return _measure(object_counts, sum_notnull),
+    return _measure(object_counts, sum_notnull)
 
 
 def _nan_to_zero(im):
@@ -169,12 +168,5 @@ def _measure(num_objs, sum_notnull):
         raise ValueError("array of object counts is empty")
 
     sum_vals = np.sum(num_objs)
-    # TODO: delete the if statement. Reasons:
-    #  - the intention to ignore images with just a couple of noise pixels should be addressed at the very beginning
-    #    of the measure_of_chaos function
-    #  - it is buggy: the number of objects is not necessarily monotonic, e.g. [1, 3, 2] would get a score of 0
-    #    because its mean is equal to the last value just by coincidence
-    if sum_vals == nlevels * num_objs[-1]:  # all objects are in the highest level
-        sum_vals = 0
     measure_value = 1 - float(sum_vals) / (sum_notnull * nlevels)
     return measure_value
