11856c15d8ae8e1dcfb94f797ed92bf5fbd3cd72
Dominik Fay <dominik.fay@gmx.de>
2015-12-15 16:22:32 +0100

    docs and pep8

--- image_measures/isotope_image_correlation.py
+++ image_measures/isotope_image_correlation.py
@@ -5,17 +5,18 @@ import numpy as np
 
 def isotope_image_correlation(images_flat, weights=None):
     """
-    Function for calculating a weighted average measure of image correlation with the principle image
-    :param images: numpy array of pixel intensities
+    Function for calculating a weighted average measure of image correlation with the principle image.
+
+    :param images_flat: numpy array of pixel intensities
     :param weights: weighting to put on each im
-    :param theoretical_isotope_intensity:
     :return: measure_value
     """
 
     if len(images_flat) < 2:
         return 0
     else:
-        iso_correlation = np.corrcoef(images_flat)[1:,0]  # slightly faster to compute all correlations and pull the elements needed
+        # slightly faster to compute all correlations and pull the elements needed
+        iso_correlation = np.corrcoef(images_flat)[1:, 0]
         # when all values are the same (e.g. zeros) then correlation is undefined
         iso_correlation[np.isinf(iso_correlation)] = 0
         return np.average(iso_correlation, weights=weights)
