8d48eb117cc00300e63dc5b76b549b971affd309
andy-d-palmer <palmer@embl.de>
2016-02-24 11:27:19 +0100

    comments

--- image_measures/isotope_pattern_match.py
+++ image_measures/isotope_pattern_match.py
@@ -1,6 +1,5 @@
 ''' Function for calculating a measure of image noise using level sets
-# Inputs: image - numpy array of pixel intensities
-         nlevels - number of levels to calculate over (note that we approximating a continuious distribution with a 'large enough' number of levels)
+# Inputs: images - numpy array of pixel intensities
 # Outputs: measure_value
  
 This function calculates a match between a set of isotope ion images and a theoretical intenisty vector
