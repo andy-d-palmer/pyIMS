9947dfcbc5aee9124fb75be69e6056ca5bd86361
andy-d-palmer <palmer@embl.de>
2016-07-20 09:12:39 +0200

    bug fix: imzml variable was being used for input and output

--- pyImagingMSpec/convert/imzml.py
+++ pyImagingMSpec/convert/imzml.py
@@ -1,5 +1,5 @@
 __author__ = 'palmer'
-import sys
+import argparse
 import numpy as np
 
 def centroid_imzml(input_filename, output_filename, step=[], apodization=False, w_size=10, min_intensity=1e-5, prevent_duplicate_pixels=False):
@@ -18,15 +18,15 @@ def centroid_imzml(input_filename, output_filename, step=[], apodization=False, 
     from pyimzml.ImzMLWriter import ImzMLWriter
     from pyMSpec.centroid_detection import gradient
 
-    imzml = ImzMLParser(input_filename)
+    imzml_in = ImzMLParser(input_filename)
 
-    mz_dtype = imzml.mzPrecision
-    int_dtype = imzml.intensityPrecision
+    mz_dtype = imzml_in.mzPrecision
+    int_dtype = imzml_in.intensityPrecision
     # Convert coords to index -> kinda hacky
-    coords = np.asarray(imzml.coordinates).round(5)
+    coords = np.asarray(imzml_in.coordinates).round(5)
     coords -= np.amin(coords, axis=0)
     if step == []:  # have a guesss
-        step = np.array([np.median(np.diff(np.unique(coords[:, i]))) for i in range(3)])
+        step = np.array([np.median(np.diff(np.unique(coords[:, i]))) for i in np.shape(coords,1)])
         step[np.isnan(step)] = 1
     print 'estimated pixel size: {} x {}'.format(step[0], step[1])
     coords = coords / np.reshape(step, (3,)).T
@@ -36,28 +36,30 @@ def centroid_imzml(input_filename, output_filename, step=[], apodization=False, 
     if prevent_duplicate_pixels:
         b = np.ascontiguousarray(coords).view(np.dtype((np.void, coords.dtype.itemsize * coords.shape[1])))
         _, coord_idx = np.unique(b, return_index=True)
-        print np.shape(imzml.coordinates), np.shape(coord_idx)
+        print np.shape(imzml_in.coordinates), np.shape(coord_idx)
 
         print "original number of spectra: {}".format(len(coords))
     else:
         coord_idx = range(len(coords))
     n_total = len(coord_idx)
     print 'spectra to write: {}'.format(n_total)
-    with ImzMLWriter(output_filename, mz_dtype=mz_dtype, intensity_dtype=int_dtype) as imzml:
+    with ImzMLWriter(output_filename, mz_dtype=mz_dtype, intensity_dtype=int_dtype) as imzml_out:
         done = 0
-        for key in range(np.shape(imzml.coordinates)[0]):
+        for key in range(np.shape(imzml_in.coordinates)[0]):
             print key
             if all((prevent_duplicate_pixels, key not in coord_idx)):  # skip duplicate pixels
                 continue
-            mzs, intensities = imzml.get_spectrum(key)
+            mzs, intensities = imzml_in.get_spectrum(key)
             if apodization:
                 from pyMSpec import smoothing
                 # todo - add to processing list in imzml
                 mzs, intensities = smoothing.apodization(mzs, intensities, {'w_size':w_size})
             mzs_c, intensities_c, _ = gradient(mzs, intensities, min_intensity=min_intensity)
             pos = coords[key]
+            if len(pos)==2:
+                pos.append(0)
             pos = (pos[0], nrow - 1 - pos[1], pos[2])
-            imzml.addSpectrum(mzs_c, intensities_c, pos)
+            imzml_out.addSpectrum(mzs_c, intensities_c, pos)
             done += 1
             if done % 1000 == 0:
                 print "[%s] progress: %.1f%%" % (input_filename, float(done) * 100.0 / n_total)
@@ -65,4 +67,8 @@ def centroid_imzml(input_filename, output_filename, step=[], apodization=False, 
 
 
 if __name__ == '__main__':
-    centroid_imzml(sys.argv[1], sys.argv[1][:-3] + "_centroid.imzML")
+    parser = argparse.ArgumentParser(description="convert centroids from profile imzml to centroided imzML")
+    parser.add_argument('input', type=str, help="input imzml file")
+    parser.add_argument('output', type=str, help="output filename (centroided imzml)")
+    args = parser.parse_args()
+    centroid_imzml(args.input, args.output)
